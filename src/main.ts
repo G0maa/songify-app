0af503c66e4c92fd57b60196bd68e50edcd752c9
G0maa <midomaxgomaa@gmail.com>
2023-04-16 16:41:08 +0200

    fix(requestLogger): bug fixed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { requestLogger } from './common/middleware/reguestLogger.middleware';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -10,9 +9,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
-  // to-do will eventually need to use winston
-  // and/or move to an injectable service
-  configService.get('NODE_ENV') === 'dev' ? app.use(requestLogger) : null;
 
   // API Docs
   const config = new DocumentBuilder()
