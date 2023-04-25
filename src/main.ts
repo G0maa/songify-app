cee67f46f3546380d8ebfc020b35915d41810163
G0maa <midomaxgomaa@gmail.com>
2023-04-25 16:31:16 +0200

    trivial(cors): setting cors to default settings

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
