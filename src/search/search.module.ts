14fceda18ad428428250c6d4a0ea27d1df3831ff
G0maa <midomaxgomaa@gmail.com>
2023-05-09 19:32:41 +0300

    Trivial(env): Make modules use env variables
    - Particularly: search and message-queue

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { ConfigService } from '@nestjs/config';
 
+// https://github.com/nestjs/elasticsearch | Ctrl + F => "Use class"
 @Module({
   imports: [
-    ElasticsearchModule.register({
-      node: 'https://localhost:9200',
-      auth: {
-        username: 'elastic',
-        password: 'changeme',
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
+    ElasticsearchModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        node: configService.get<string>('ELASTICSEARCH_URL'),
+        auth: {
+          username: configService.get<string>('ELASTICSEARCH_USERNAME'),
+          password: configService.get<string>('ELASTICSEARCH_PASSWORD'),
+        },
+        tls: {
+          rejectUnauthorized: false, // because self-signed certificate or other reasons.
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [SearchController],
