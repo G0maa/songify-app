cc5faa65cb3f3f1611ce67ce8f76724df95f3637
G0maa <midomaxgomaa@gmail.com>
2023-05-13 11:51:20 +0300

    feat(recommend): v1 using .csv file done

--- src/recommend/recommend.controller.ts
+++ src/recommend/recommend.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Version } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { RecommendService } from './recommend.service';
 import { JwtGuard } from 'src/auth/guard';
@@ -9,6 +9,21 @@ import { GetUser } from 'src/auth/decorator';
 export class RecommendController {
   constructor(private recommendService: RecommendService) {}
 
+  // Which is more RESTful? (similar to the favorites situation)
+  // is it: GET /recommend
+  // or GET /user/{user_id}/recommend
+  // or GET /recommend/{user_id}
+  // or GET /user/recommend (since userId is in the token)
+  @Version('1')
+  @UseGuards(JwtGuard)
+  @ApiBearerAuth()
+  @Get()
+  getRecommend(@GetUser('id') id: number) {
+    return this.recommendService.getRecommendedV1(id);
+  }
+
+  // under-development.
+  @Version('2')
   @UseGuards(JwtGuard)
   @ApiBearerAuth()
   @Get()
--- src/recommend/recommend.service.ts
+++ src/recommend/recommend.service.ts
@@ -2,13 +2,72 @@ import { Injectable } from '@nestjs/common';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TrackHistory } from '@prisma/client';
+// Personally I believe a controller/service should not deal with files.
+import * as fs from 'fs';
+import { parse } from 'csv-parse'; // Currently a dev dependency.
 
 @Injectable()
 export class RecommendService {
+  private recommend = [];
+
   constructor(
     private readonly amqpConnection: AmqpConnection,
     private prismaService: PrismaService,
-  ) {}
+  ) {
+    // loadRecommend is async, how does NestJS deal with that?
+    // https://docs.nestjs.com/fundamentals/async-providers
+    // this feels pretty wrong.
+    this.loadRecommend().then((res) => {
+      this.recommend = res;
+    });
+  }
+
+  async getRecommendedV1(id: number) {
+    // 1. get all tracks in history
+    // [{ trackId: 1 }, ...}]
+    const history = await this.prismaService.trackHistory.findMany({
+      where: { userId: id },
+      select: { trackId: true },
+    });
+
+    // Note: This isn't accurate since you can listen to the same track 5 times...
+    if (history.length < 5) {
+      return {
+        message:
+          'History enteries are not enough to recommend, must listen to 5+ unique tracks.',
+      };
+    }
+
+    // 2. get all recommended tracks
+    const recommendSet = new Set<number>();
+    for (const record of history) {
+      const arr: [] = this.recommend[record.trackId];
+      for (const element of arr) {
+        recommendSet.add(element);
+      }
+    }
+
+    // print list of recommended tracks
+    // note: if the track isn't found in DB i.e. using the sample data,
+    // the code will run normally, without getting the track.
+    console.log('recommendSet: ', recommendSet);
+
+    // 3. fetch all recommended tracks
+    const recommendArr = Array.from(recommendSet);
+    const recommend = await this.prismaService.track.findMany({
+      where: { id: { in: recommendArr } },
+      select: {
+        id: true,
+        title: true,
+        genre: true,
+        duration: true,
+        releaseDate: true,
+        artist: { select: { name: true } },
+      },
+    });
+
+    return recommend;
+  }
 
   async getRecommended(id: number) {
     // to-do: but is the track favorited?
@@ -48,5 +107,22 @@ export class RecommendService {
       timeout: 3000,
     });
     return response;
+  }
+
+  private async loadRecommend() {
+    const parser = fs
+      .createReadStream('./prisma/recommend_sample.csv')
+      .pipe(parse({ columns: true }));
+
+    const recommend = [];
+    for await (const record of parser) {
+      const id = +record['id'] + 1;
+      const nearestSongs = await JSON.parse(record['nearest_songs']);
+
+      // note: id is converted to a string again here.
+      recommend[id] = nearestSongs;
+    }
+
+    return recommend;
   }
 }
